dff6207291cd7ddfdad1cf654fc1b2673f7da7fe
Marius Sebastian Trif <coolsebz@gmail.com>
2015-10-22 02:58:12 +0300

    Added more content to the expenses controller

--- modules/expenses/server/controllers/expenses.server.controller.js
+++ modules/expenses/server/controllers/expenses.server.controller.js
@@ -14,7 +14,7 @@ exports.create = function(req, res) {
 	expense.user_id = req.user.id;
 
 	expense.save().then(function(savedExpense) {
-		res.json(article);		
+		res.json(savedExpense);		
 	}).catch(function(err) {
 		return res.status(400).send({
 			message: errorHandler.getErrorMessage(err);
@@ -22,11 +22,29 @@ exports.create = function(req, res) {
 	});
 };
 
+/* 
+ * Show an expense
+ */
 exports.read = function(req, res) {
-	
+	res.json(req.expense);
 };
 
 exports.update = function(req, res) {
+
+  var expense = req.expense;
+
+  expense.title = req.body.title;
+  expense.value = req.body.value;
+
+  //note(seb): setting the method explicitly to 'update' makes it so that if it fails we get a different,
+  //           more accurate error message back than just 'no rows updated'
+  expense.save({ method: 'update' }).then(function(savedExpense) {
+    res.json(savedExpense);
+  }).catch(function(err) {
+    return res.status(400).send({
+      message: errorhandler.getErrorMessage(err);
+    })
+  });
 
 };
 
@@ -40,6 +58,7 @@ exports.list = function(req, res) {
 
 exports.expenseById = function(req, res, next, id) {
 
-	
+
+
 
 };
